6134605cb6708ddb66852ce89c738faa345c3f9e
Ben <brbryant2639@gmail.com>
2022-01-14 12:05:27 -0600

    UPDATE: objects walkthrough

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,47 @@
 console.log("Hello World!");
 
-// Given a tax rate and subtotal returns the total price amount
-function calcTotal(subtotal, taxRate) {
-  let tax = subtotal * taxRate;
-  return subtotal + tax;
-}
+const mugWarmer = {
+  brand: "Kin",
+  color: "black",
+  shape: "circle",
+  isOn: false,
+  level: null,
+  toggleOn: function () {
+    this.isOn = !this.isOn;
+    console.log(`Mug Warmer turning ${this.isOn}...`);
+  },
+  toggleLevel: function () {
+    if (!this.isOn) {
+      this.toggleOn();
+    }
 
-console.log(calcTotal(10, 0.1));
-console.log(calcTotal(22, 0.15));
-console.log(calcTotal(5, 0.20));
+    this.level++;
+
+    if (this.level > 3) {
+      this.toggleOn();
+      this.level = null;
+    }
+    console.log(`Level is ${this.level}...`);
+  },
+};
+
+mugWarmer.toggleLevel();
+mugWarmer.toggleLevel();
+mugWarmer.toggleLevel();
+mugWarmer.toggleLevel();
+mugWarmer.toggleLevel();
+
+let name = "Ben";
+
+name; // string data type, no properties or methods
+// turns the string into a string object (polymorphism), to access the length property
+name.length; // 3
+name[1]; // "e"
+name.slice(1); // "en"
+name.toUpperCase(); // "BEN"
+
+const scores = [1, 2, 3, 4, 5];
+
+scores.forEach(function (num) {
+  console.log(num);
+});
